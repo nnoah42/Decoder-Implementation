1f451dafd38ec582880090a1810178bbeea8c169
nnoah42 <nk67@ksu.edu>
2026-09-15 21:56:42 -0500

    update test.py to work with the new updates

--- test.py
+++ test.py
@@ -2,9 +2,10 @@ import tiktoken
 
 from encode import encode_text as encode
 from blocks import create_block_batches
-from token_embedding import token_embedding
+from input_embedding import InputEmbedding
 
 encoding = tiktoken.get_encoding("gpt2")
 tokenized_data = encode("the-verdict.txt", "gpt2")
 blocks = create_block_batches(tokenized_data, block_size=8)
-token_embeddings = token_embedding(numdim=8, encodings=blocks, vocab_size=encoding.n_vocab)
+input_embedding = InputEmbedding(numdim=8, block_size=8, vocab_size=encoding.n_vocab)
+embeddings = input_embedding(blocks)
